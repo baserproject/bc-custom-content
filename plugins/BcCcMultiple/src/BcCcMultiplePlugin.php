66ffc9380182d032161d12c4a75050e91fca22e8
ryuring <ryuring55@gmail.com>
2024-04-25 06:28:34 +0000

    open 5.1-dev

--- plugins/BcCcMultiple/src/BcCcMultiplePlugin.php
+++ plugins/BcCcMultiple/src/BcCcMultiplePlugin.php
@@ -9,7 +9,7 @@
  * @license       https://basercms.net/license/index.html MIT License
  */
 
-namespace BcCcEmail;
+namespace BcCcMultiple;
 
 use BaserCore\Annotation\UnitTest;
 use BaserCore\Annotation\NoTodo;
@@ -20,7 +20,7 @@ use BaserCore\BcPlugin;
 /**
  * Plugin
  */
-class Plugin extends BcPlugin
+class BcCcMultiplePlugin extends BcPlugin
 {
 
     /**
