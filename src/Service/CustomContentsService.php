9566fbe47ddb2ac5a863ea3252efefd36317e95a
ryuring <ryuring55@gmail.com>
2024-08-22 07:42:12 +0000

    composer.json の依存関係を調整

--- src/Service/CustomContentsService.php
+++ src/Service/CustomContentsService.php
@@ -13,13 +13,13 @@ namespace BcCustomContent\Service;
 
 use BaserCore\Error\BcException;
 use BaserCore\Utility\BcContainerTrait;
+use BaserCore\Utility\BcFolder;
 use BaserCore\Utility\BcUtil;
 use BcCustomContent\Model\Table\CustomContentsTable;
 use Cake\Core\App;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
 use Cake\Datasource\EntityInterface;
-use Cake\Filesystem\Folder;
 use Cake\ORM\Query;
 use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
@@ -113,10 +113,10 @@ class CustomContentsService implements CustomContentsServiceInterface
         if ($options['status'] === 'publish') {
             $conditions = $this->CustomContents->Contents->getConditionAllowPublish();
         }
-        return $this->CustomContents->get($id, [
-            'contain' => ['Contents' => ['Sites']],
-            'conditions' => $conditions
-        ]);
+        return $this->CustomContents->get($id,
+            contain:  ['Contents' => ['Sites']],
+            conditions: $conditions
+        );
     }
 
     /**
@@ -239,9 +239,9 @@ class CustomContentsService implements CustomContentsServiceInterface
     {
         $list = ['id' => 'No', 'published' => __d('baser_core', '公開日付'), 'created' => __d('baser_core', '登録日'), 'modified' => __d('baser_core', '編集日')];
         if(!$tableId) return $list;
-        $table = $this->CustomContents->CustomTables->get($tableId, ['contain' => [
+        $table = $this->CustomContents->CustomTables->get($tableId, contain: [
             'CustomLinks' => ['CustomFields']
-        ]]);
+        ]);
         if($table->custom_links) {
             foreach($table->custom_links as $customLink) {
                 if($customLink->custom_field->status && $customLink->custom_field->type !== 'group') {
@@ -276,13 +276,13 @@ class CustomContentsService implements CustomContentsServiceInterface
         $templates = [];
         foreach($templatesPaths as $templatePath) {
             $templatePath .= 'CustomContent' . DS;
-            $folder = new Folder($templatePath);
-            $files = $folder->read(true, true);
-            if ($files[0]) {
+            $folder = new BcFolder($templatePath);
+            $files = $folder->getFolders();
+            if ($files) {
                 if ($templates) {
-                    $templates = array_merge($templates, $files[0]);
+                    $templates = array_merge($templates, $files);
                 } else {
-                    $templates = $files[0];
+                    $templates = $files;
                 }
             }
         }
